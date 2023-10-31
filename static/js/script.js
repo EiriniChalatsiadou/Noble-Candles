0df4518a8b105c5e98b94a080840b2c1710b8c59
EiriniChalatsiadou <>
2023-10-31 15:16:32 +0000

    add google maps code

---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+/* jshint esversion: 6 */
+/* globals $:false */
+
+if (window.location.pathname == "/") {
+    //initialise map
+    const initMap = () => {
+        // The location of Uluru
+        const uluru = { lat: 53.3498, lng: -6.2603 };
+        // The map, centered at Uluru
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 4,
+            center: uluru,
+        });
+        // The marker, positioned at Uluru
+        new google.maps.Marker({
+            position: uluru,
+            map: map,
+        });
+    };
+    initMap();
+}
