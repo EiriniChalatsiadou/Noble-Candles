fb475c8837aae0135b9b6a96cd29e8686e3c35fc
EiriniChalatsiadou <>
2023-11-10 14:59:11 +0000

    added products fixtures

--- products/views.py
+++ products/views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import render
-# from .models import Product
+from .models import Product
 
 
 def all_products(request):
 
-    # products = Product.objects.all()
+    products = Product.objects.all()
 
     context = {
-        # 'products': products,
-        'products': [
-            {
-                'name': 'Secret Rose',
-                'sku': '1234',
-                'category': 'candles',
-                'description': 'soya wax candle, floral-vetiner-sandalwood',
-                'price': '15,00',
-                'rating': '4,5',
-                'image_url': "/media/products/candle-1.jfif",
-                'image': '1',
-            }
-        ]
+        'products': products,
+
     }
     return render(request, 'products/products.html', context)
