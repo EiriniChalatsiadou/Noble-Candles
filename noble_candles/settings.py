77c26c99579eab54c885839a3c3c40089d433750
EiriniChalatsiadou <>
2023-11-12 01:04:20 +0000

    added cart page

--- noble_candles/settings.py
+++ noble_candles/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context.cart_contents'
             ],
         },
     },
@@ -165,3 +166,12 @@ media_URL = '/media/'
 media_ROOT = os.path.join(BASE_DIR, 'media')
 
 mimetypes.add_type("text/javascript", ".js", True)
+
+
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
