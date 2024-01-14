b0d7a3bae980763196f2e767bc3d554a95194ae9
EiriniChalatsiadou <>
2024-01-14 19:37:20 +0000

    message details view changes. Changed menu text colors

--- customer_messages/urls.py
+++ customer_messages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import message_list_view, message_delete_view
+from .views import message_list_view, message_delete_view, message_details
 
 urlpatterns = [
     path('', message_list_view, name='customer_messages'),
-    path('delete/<int:message_id>/', message_delete_view, name='message-delete'),
+    path('<int:message_id>/', message_details, name='message_details'),
+    path('delete/<int:message_id>/', message_delete_view, name='message_delete'),
 
 ]
--- customer_messages/views.py
+++ customer_messages/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
-from django.views import View
-from django.views.generic import ListView
-from django.views.generic.edit import DeleteView
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.urls import reverse_lazy
 from .models import CustomerMessage
 
@@ -13,6 +11,18 @@ def message_list_view(request):
     }
     
     return render(request, 'customer_messages/message_list.html', context)
+
+def message_details(request, message_id):
+
+    try:
+        message = get_object_or_404(CustomerMessage, pk=message_id)
+    except Http404:
+        return render(request, '404.html')
+
+    context = {
+        'message': message,
+    }
+    return render(request, 'customer_messages/message_details.html', context)
 
 def message_delete_view(request):
     model = CustomerMessage
