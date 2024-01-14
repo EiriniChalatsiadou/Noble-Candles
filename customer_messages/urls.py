10e969b1756246880c9c23d1381e2d15e618cf7f
EiriniChalatsiadou <>
2024-01-14 00:37:20 +0000

    run migrations and trying to add list message view

---
+++ customer_messages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import message_list_view, message_delete_view
+
+urlpatterns = [
+    path('', message_list_view.as_view(), name='customer_messages'),
+    path('delete/<int:message_id>/', message_delete_view.as_view(), name='message-delete'),
+
+]
--- customer_messages/views.py
+++ customer_messages/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
+from .models import CustomerMessage
 
-# Create your views here.
+class message_list_view(ListView):
+    model = CustomerMessage
+    template_name = 'customer_messages/message_list.html'
+
+class message_delete_view(DeleteView):
+    model = CustomerMessage
+    template_name = 'customer_messages/message_confirm_delete.html'
+    success_url = reverse_lazy('message-list')  # Redirect to the message list after deletion
+
