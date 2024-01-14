a71adb7981ff6017979c99aee0f5fa1e7ba14544
EiriniChalatsiadou <>
2024-01-14 16:02:13 +0000

    list messages view

--- customer_messages/urls.py
+++ customer_messages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import message_list_view, message_delete_view
 
 urlpatterns = [
-    path('', message_list_view.as_view(), name='customer_messages'),
-    path('delete/<int:message_id>/', message_delete_view.as_view(), name='message-delete'),
+    path('', message_list_view, name='customer_messages'),
+    path('delete/<int:message_id>/', message_delete_view, name='message-delete'),
 
 ]
--- customer_messages/views.py
+++ customer_messages/views.py
@@ -5,11 +5,16 @@ from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy
 from .models import CustomerMessage
 
-class message_list_view(ListView):
-    model = CustomerMessage
-    template_name = 'customer_messages/message_list.html'
+def message_list_view(request):
+    messages = CustomerMessage.objects.all()
 
-class message_delete_view(DeleteView):
+    context = {
+        'messages': messages,
+    }
+    
+    return render(request, 'customer_messages/message_list.html', context)
+
+def message_delete_view(request):
     model = CustomerMessage
     template_name = 'customer_messages/message_confirm_delete.html'
     success_url = reverse_lazy('message-list')  # Redirect to the message list after deletion
