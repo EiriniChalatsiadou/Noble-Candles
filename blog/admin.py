bb3251041c70fa958628b1f83ffa5b23c9d3dc91
EiriniChalatsiadou <>
2024-01-15 08:45:22 +0000

    added Blog posts to django admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import BlogPost
+
+class BlogPostMessageAdmin(admin.ModelAdmin):
+    list_display = ('date_created', 'date_updated', 'title', 'content', 'published','image_url', 'author')
+    ordering = ('date_created',)
 
 # Register your models here.
+admin.site.register(BlogPost, BlogPostMessageAdmin)
