b45ec7e497a642a86b7077c4274a86a52b60ba06
EiriniChalatsiadou <>
2024-01-15 00:58:31 +0000

    blog details view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import all_blog_posts
+from .views import all_blog_posts, blogpost_details
 
 urlpatterns = [
-    path('', all_blog_posts, name='all_blog_posts'),
+    path('', all_blog_posts, name='all_blogposts'),
+    path('<int:blogpost_id>/', blogpost_details, name='blogpost_details'),
 ]
 
 handler404 = 'noble_candles.view.handler404'
--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,18 @@ def all_blog_posts(request):
         'published_blogposts': published_blogposts,
     }
     return render(request, 'blog/blogposts_list.html', context)
+
+
+def blogpost_details(request, blogpost_id):
+
+    blogpost = get_object_or_404(BlogPost, pk=blogpost_id)
+
+    # Check if the blog post is published
+    if not blogpost.published:
+        # show 404 if unpublished
+        return render(request, '404.html')
+
+    context = {
+        'blogpost': blogpost,
+    }
+    return render(request, 'blog/blogpost_details.html', context)
